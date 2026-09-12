fc483078b327c7861ea387546f67b1c4a39af20e
Talha Habib <talhahabib842653@gmail.com>
2026-09-12 12:03:21 +0500

    CCA project reEdited

--- FullStackCCA/online-learning-backend/routes/api.php
+++ FullStackCCA/online-learning-backend/routes/api.php
@@ -87,4 +87,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+   
+
 });
